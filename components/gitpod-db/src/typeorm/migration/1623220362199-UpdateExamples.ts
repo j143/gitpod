2b145a9c6c97a402adfbf96ac118c5f5ca211216
Geoffrey Huntley <ghuntley@ghuntley.com>
2021-06-09 08:23:04 +0000

    add all future options at 99 and will require adjustments to the sort in the next couple of commits

--- components/gitpod-db/src/typeorm/migration/1623220362199-UpdateExamples.ts
+++ components/gitpod-db/src/typeorm/migration/1623220362199-UpdateExamples.ts
@@ -10,12 +10,23 @@ export class UpdateExamples1623220362199 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
         const newEntries = [
-            { url: 'https://github.com/gitpod-io/example-typescript-node', description: 'A Node.js app written in TypeScript.', priority: 90 },
-            { url: 'https://github.com/gitpod-io/example-python-django', description: 'A Django app written in Python. ', priority: 70 },
-            { url: 'https://github.com/gitpod-io/example-golang-cli', description: 'A CLI starter written in Go.', priority: 40 },
-            { url: 'https://github.com/gitpod-io/example-rust-cli', description: 'A CLI starter written in Rust.', priority: 30 },
-            { url: 'https://github.com/gitpod-io/spring-petclinic', description: 'A Spring app written in Java.', priority: 20 },
-            { url: 'https://github.com/gitpod-io/sveltejs-template', description: 'A Svelte.js app writtten in JavaScript', priority: 10 },
+            { url: 'https://github.com/gitpod-io/template-typescript-node', description: 'A Node.js app written in TypeScript.', priority: 99 },
+            { url: 'https://github.com/gitpod-io/template-typescript-node-expressjs', description: 'A Express.js for Node.js app written in TypeScript.', priority: 99 },
+            { url: 'https://github.com/gitpod-io/template-typescript-react', description: '', priority: 99 },
+
+            { url: 'https://github.com/gitpod-io/template-php-laravel', description: '', priority: 99 },
+            { url: 'https://github.com/gitpod-io/template-php-drupal', description: '', priority: 99 },
+            { url: 'https://github.com/gitpod-io/template-python-flask', description: '', priority: 99 },
+
+            { url: 'https://github.com/gitpod-io/template-golang-cli', description: 'A CLI starter written in Go.', priority: 99 },
+            { url: 'https://github.com/gitpod-io/template-rust-cli', description: 'A CLI starter written in Rust.', priority: 99 },
+            { url: 'https://github.com/gitpod-io/template-python-django', description: 'A Django app written in Python. ', priority: 99 },
+            { url: 'https://github.com/gitpod-io/template-ruby-on-rails', description: 'A Ruby on Rails app written in Ruby. ', priority: 99 },
+
+            { url: 'https://github.com/gitpod-io/template-dotnet-core-cli-csharp', description: 'A CLI starter for .NET written in C#', priority: 99 },
+            { url: 'https://github.com/gitpod-io/template-sveltejs', description: 'A Svelte.js app writtten in JavaScript', priority: 99 },
+            { url: 'https://github.com/gitpod-io/spring-petclinic', description: 'A Spring app written in Java.', priority: 99 },
+
         ]
         // delete old entries
         await queryRunner.query("DELETE FROM d_b_repository_white_list");
