5fccbaf806a3dad3e20a4c41bc39acbfef06984a
Geoffrey Huntley <ghuntley@ghuntley.com>
2021-06-09 06:43:43 +0000

    werft will not allow builds unless there is a header. i like this aspect of werft

--- components/gitpod-db/src/typeorm/migration/1623220362199-UpdateExamples.ts
+++ components/gitpod-db/src/typeorm/migration/1623220362199-UpdateExamples.ts
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2021 Gitpod GmbH. All rights reserved.
+ * Licensed under the GNU Affero General Public License (AGPL).
+ * See License-AGPL.txt in the project root for license information.
+ */
+
 import {MigrationInterface, QueryRunner} from "typeorm";
 
 export class UpdateExamples1623220362199 implements MigrationInterface {
